48f035e540aebb7c3d4f60a629b6ccda307910f2
CennoxX <cesar.bernard@gmx.de>
2019-03-07 14:35:44 +0100

    exit program

--- Reconcile2.cs
+++ Reconcile2.cs
@@ -72,6 +72,7 @@ namespace reconcile2
             if (args.Length != 5)
             {
                 Console.WriteLine("format: dotnet run <input file 1> <input row 1> <input file 2> <input row 2> <output file>");
+                Environment.Exit(-1);
             }
             else
             {
